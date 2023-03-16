5d9717bc9dfbf777b4b1d7af89012ca42a920242
10kdream <97788884+10kdream@users.noreply.github.com>
2023-03-16 07:30:30 +0100

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -173,7 +173,7 @@ const Home = (props) => {
           <div className="home-slider-container blaze-container">
             <div className="home-slider-track-container blaze-track-container">
               <div className="home-slider-track blaze-track">
-                <div data-role="current-banner" className="banner home-slide">
+                <div data-role="current-banner" className="home-slide banner">
                   <img
                     alt="image"
                     src="https://freight.cargo.site/w/600/q/75/i/c27be1f3d6f7a6dd593ef7e44ca43a5b5d37df89bdcbcbc113c869bb1488bf6a/a1392942460_10.jpeg"
@@ -183,36 +183,22 @@ const Home = (props) => {
                 <div data-role="current-banner" className="banner home-slide1">
                   <img
                     alt="image"
-                    src="https://freight.cargo.site/w/600/q/75/i/c27be1f3d6f7a6dd593ef7e44ca43a5b5d37df89bdcbcbc113c869bb1488bf6a/a1392942460_10.jpeg"
+                    src="https://freight.cargo.site/w/600/q/75/i/365505ee7c320fca0cd64419a231a723720fb348bacd9a6de151f994ef3386fd/kotakotakota.png"
                     className="home-image04"
                   />
                 </div>
                 <div data-role="current-banner" className="banner home-slide2">
                   <img
                     alt="image"
-                    src="https://freight.cargo.site/w/600/q/75/i/365505ee7c320fca0cd64419a231a723720fb348bacd9a6de151f994ef3386fd/kotakotakota.png"
+                    src="https://freight.cargo.site/w/600/q/75/i/7ffbe96d7bcadf082faee49e485b8ff6dbf69738fdc7b196ba26144b11ee15c9/mema-estou.jpeg"
                     className="home-image05"
                   />
                 </div>
                 <div data-role="current-banner" className="banner home-slide3">
                   <img
                     alt="image"
-                    src="https://freight.cargo.site/w/600/q/75/i/365505ee7c320fca0cd64419a231a723720fb348bacd9a6de151f994ef3386fd/kotakotakota.png"
-                    className="home-image06"
-                  />
-                </div>
-                <div data-role="current-banner" className="banner home-slide4">
-                  <img
-                    alt="image"
-                    src="https://freight.cargo.site/w/600/q/75/i/7ffbe96d7bcadf082faee49e485b8ff6dbf69738fdc7b196ba26144b11ee15c9/mema-estou.jpeg"
-                    className="home-image07"
-                  />
-                </div>
-                <div data-role="current-banner" className="banner home-slide5">
-                  <img
-                    alt="image"
                     src="https://freight.cargo.site/w/600/q/75/i/01d4d8ebb25c394ba53de81eef6ff759c150f1c801ebf8df5f677f7d1be4ae6a/fullacurve.jpg"
-                    className="home-image08"
+                    className="home-image06"
                   />
                 </div>
               </div>
@@ -410,7 +396,7 @@ new BlazeSlider(el, {
           <img
             alt="image"
             src="/playground_assets/growth-1400w.png"
-            className="home-image09"
+            className="home-image07"
           />
         </div>
       </section>
@@ -420,7 +406,7 @@ new BlazeSlider(el, {
           <img
             alt="image"
             src="/playground_assets/experience-1400w.png"
-            className="home-image10"
+            className="home-image08"
           />
         </div>
         <div className="home-content04">
@@ -719,11 +705,11 @@ new BlazeSlider(el, {
           </p>
         </div>
         <div className="home-content07">
-          <div className="home-image11">
+          <div className="home-image09">
             <img
               alt="image"
               src="/playground_assets/data-1400w.png"
-              className="home-image12"
+              className="home-image10"
             />
           </div>
           <div className="home-accordion">
@@ -1062,7 +1048,7 @@ new BlazeSlider(el, {
               <img
                 alt="image"
                 src="/playground_assets/logo.svg"
-                className="home-image13"
+                className="home-image11"
               />
               <span className="home-text71">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -1149,7 +1135,7 @@ new BlazeSlider(el, {
               <img
                 alt="image"
                 src="/playground_assets/arrow.svg"
-                className="home-image14"
+                className="home-image12"
               />
             </button>
           </div>
